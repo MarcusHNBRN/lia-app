92a651a7aca8c3af0ad421ee109435f22e2927f0
Marcus Heneborn <heneborn@gmail.com>
2024-03-27 13:51:54 +0100

    1st commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('dashboard');
+        }
+        return redirect()->back()->with('error', 'Invalid credentials.');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<form action="/login" method="post">
+    @csrf
+    <div>
+        <label for="email">Email</label>
+        <input name="email" id="email" type="email" />
+    </div>
+    <div>
+        <label for="password">Password</label>
+        <input name="password" id="password" type="password" />
+    </div>
+    <button type="submit">Login</button>
+    @include('errors')
+</form>
