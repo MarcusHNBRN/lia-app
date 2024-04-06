61016971d5d24a82e3fe7e97db34cea1ae354ccd
selmarosine <selma.gronsten@hotmail.se>
2024-04-06 23:25:13 +0200

    start styling for gallery

--- lia-app/resources/views/studentGallery.blade.php
+++ lia-app/resources/views/studentGallery.blade.php
@@ -3,10 +3,16 @@
 @php
 $students = DB::table('students')->get();
 @endphp
+<link rel="stylesheet" href="{{ asset('css/gallery.css') }}" />
 
-<main>
+<div class="studentMain">
 @foreach ($students as $student)
+<div class="profileCard">
     <p> {{ $student->studentName }} </p>
+    <p> {{ $student->WU }} </p>
+    <p>sKILLS:</p>
+    <p> {{ $student->skills }} </p>
+</div>
 
 @endforeach
-</main>
+</div>
