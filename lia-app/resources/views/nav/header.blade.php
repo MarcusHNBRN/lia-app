27a6465fccbca6f5de59288eeebd0f41abeb9362
selmarosine <selma.gronsten@hotmail.se>
2024-04-07 18:59:34 +0200

    header styling etc

--- lia-app/resources/views/nav/header.blade.php
+++ lia-app/resources/views/nav/header.blade.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="{{ asset('css/header.css') }}" />
-    <!--<link rel="stylesheet" type="text/css" href="/public/css/header.css" />--->
 </head>
-<body>
-<header>
-    <div class="yrgoHeader">Yrgo</div>
 
-    <div class="homeButton">home</div>
-    <div class="accountButton">you</div>
-    <div class="settingsButton">set</div>
-</header>
+<body>
+    <header>
+        <div class="headerRight">
+            <div class="yrgoHeader">Yrgo</div>
+        </div>
+        <div class="headerLeft">
+            <div class="homeButton">home</div>
+            <div class="accountButton">you</div>
+            <div class="settingsButton">set</div>
+        </div>
+    </header>
