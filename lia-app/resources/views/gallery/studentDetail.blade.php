a1c12a421dd399a12a7bd22fc0a8329866984cf8
selmarosine <selma.gronsten@hotmail.se>
2024-04-10 23:28:34 +0200

    more styling

--- lia-app/resources/views/gallery/studentDetail.blade.php
+++ lia-app/resources/views/gallery/studentDetail.blade.php
@@ -1,3 +1,25 @@
 @include('nav.header')
 
-<h1> {{ $student->studentName }} </h1>
+<link rel="stylesheet" href="{{ asset('css/gallery.css') }}" />
+
+<div class="backButton"> <a href="/"> <img src="/svgs/leftArrow.svg" alt="back"> </a> </div>
+
+<div class="studentBasicInfo">
+    <div class="studentDetailPicture"> Bild här </div>
+    <h1 class="studentDetailName"> {{ $student->studentName }} </h1>
+    <h1 class="studentDetailStudy">DD / WU</h1>
+    <h5 class="studentDetailCity">Stad</h5>
+    <div class="studentDetailAge"> {{ $student->age }} </div>
+</div>
+
+<div class="studentMoreInfo"> <!-- selma om det inte är inlagt? -->
+    <h1 class="infoTitles">Om mig</h1>
+    <h1 class="infoTitles">Utbildningar</h1>
+    <h1 class="infoTitles">Arbetslivserfarenhet</h1>
+    <h1 class="infoTitles infoTitlesLast">Intressen</h1>
+    <h1 class="infoTitles infoTitlesLast">Färdigheter</h1>
+</div>
+
+<div class="studentContactInfo">
+    
+</div>
