e8a7595446e034031adca45835185306c9fc7c7b
selmarosine <selma.gronsten@hotmail.se>
2024-04-14 21:52:38 +0200

    fixed the database connection in student gallery blade controller

--- lia-app/app/Http/Controllers/StudentGalleryController.php
+++ lia-app/app/Http/Controllers/StudentGalleryController.php
@@ -11,7 +11,14 @@ class StudentGalleryController extends Controller
     public function index()
     {
         $students = Student::all();
-        return view('gallery.studentGallery', compact('students'));
+        $studentInfos = [];
+
+        foreach ($students as $student) {
+            $studentInfo = StudentInfo::where('studentId', $student->id)->first();
+            $studentInfos[$student->id] = $studentInfo;
+        }
+
+        return view('gallery.studentGallery', compact('students', 'studentInfos'));
     }
 
     public function show($studentId)
