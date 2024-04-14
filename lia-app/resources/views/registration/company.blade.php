62322456205249402f67842e57f34918e00d8b16
Marcus Heneborn <heneborn@gmail.com>
2024-04-14 20:01:50 +0200

    registration styling

--- lia-app/resources/views/registration/company.blade.php
+++ lia-app/resources/views/registration/company.blade.php
@@ -23,61 +23,59 @@
             <textarea name="description" placeholder="Description" required></textarea>
         </div>
 
-        <div class="button-container">
-            <p class="title"> Klass
-            <p>
-            <div class="radio-button-container">
-                <label class="radio-button">
+        <div class="card-container">
+            <p class="title"> Klass </p>
+            <div class="industry-container">
+                <label class="industry-button">
                     <input type="radio" name="industry" value="Bilindustrin" required>
                     <span>Bilindustrin</span>
                 </label>
-                <label class="radio-button">
+
+                <label class="industry-button">
                     <input type="radio" name="industry" value="Media och Kommunikation" required>
                     <span>Media och Kommunikation</span>
                 </label>
-                <label class="radio-button">
+                <label class="industry-button">
                     <input type="radio" name="industry" value="Film och TV-Produktion" required>
                     <span>Film och TV-Produktion</span>
                 </label>
-                <label class="radio-button">
+                <label class="industry-button">
                     <input type="radio" name="industry" value="Annat" required>
                     <span>Annat</span>
                 </label>
             </div>
         </div>
-        <div class="button-container">
-            <p class="title"> Klass
-            <p>
+        <div class="card-container">
+            <p class="title"> Klass </p>
             <div class="radio-button-container">
-                <label class="radio-button">
+                <label class="employees-button">
                     <input type="radio" name="employees" value="1-20" required>
                     <span>1-20</span>
                 </label>
-                <label class="radio-button">
+                <label class="employees-button">
                     <input type="radio" name="employees" value="20-100" required>
                     <span>20-100</span>
                 </label>
-                <label class="radio-button">
+                <label class="employees-button">
                     <input type="radio" name="employees" value="100+" required>
                     <span>100+</span>
                 </label>
             </div>
         </div>
-
-        <div class="button-container">
-            <p class="title"> Klass
-            <p>
+        <div class="card-container">
+            <p class="title"> Klass </p>
             <div class="radio-button-container">
-                <label class="radio-button">
+                <label class="language-button">
                     <input type="radio" name="language" value="english" required>
                     <span>Engelska</span>
                 </label>
-                <label class="radio-button">
+                <label class="language-button">
                     <input type="radio" name="language" value="swedish" required>
                     <span>Svenska</span>
                 </label>
             </div>
         </div>
+
 
         <div class="input-container">
             <p class="input-title"> Länk till företagets Linkdin </p>
@@ -89,10 +87,14 @@
             <input type="url" name="homepage" placeholder="Länk till din hemsida">
         </div>
 
-        <div class="input-container">
-            <p class="input-title"> Profile Picture </p>
-            <input type="file" name="profile_picture">
+        <div class="card-container">
+            <div class="file-container">
+                <p class="file-title"> Profile Picture </p>
+                <input type="file" id="profile-picture-company-input" name="profile_picture" onchange="updateFileName('profile-picture-company-input')">
+                <label for="profile-picture-company-input" id="file-label">Choose File</label>
+            </div>
         </div>
+
         <div class="input-container">
             <p class="input-title"> Lösenord </p>
             <input type="password" name="password" placeholder="Lösenord" required>
@@ -105,3 +107,5 @@
         <button type="submit">Register</button>
     </div>
 </form>
+
+<script src="{{ asset('js/registration.js') }}"></script>
