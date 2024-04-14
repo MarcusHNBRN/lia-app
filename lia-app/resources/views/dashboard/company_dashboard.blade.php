7a2aec8bc36bc8343e2175400d539d8a1511b8a0
Marcus Heneborn <heneborn@gmail.com>
2024-04-14 12:19:16 +0200

    small fix with dashboard

--- lia-app/resources/views/dashboard/company_dashboard.blade.php
+++ lia-app/resources/views/dashboard/company_dashboard.blade.php
@@ -2,5 +2,6 @@
 <p>Hello, {{ $company->companyName }}!</p>
 <a href="{{ route('logout') }}">Logout</a>
 
-
+@if(Auth::guard('company')->check())
 <a href="{{ route('company.profile', ['id' => Auth::guard('company')->user()->id]) }}">View Profile</a>
+@endif
